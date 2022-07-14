4b73d0baa54c57d332157810c4fb59dfaa654d0c
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-07-14 12:49:45 -0400

    added savefig to full series displaying

--- displaying/examples/LENS2_full_series.py
+++ displaying/examples/LENS2_full_series.py
@@ -13,11 +13,13 @@ from scipy.stats import gamma
 # get Quinta Normal time series
 qn = se.get_QN_annual_precip()
 
-lens2 = lens.get_LENS2_annual_precip_QNEW()#.sel(run=slice(30,31))
+# get LENS2 QNEW time series
+lens2 = lens.get_LENS2_annual_precip_QNEW()
 lens2_ensmean = lens2.mean('run')
 
+# create figure
 fig = plt.figure(figsize=(16,5))
-# plt.rcParams["font.family"] = 'Arial'
+plt.rcParams["font.family"] = 'Arial'
 plt.rcParams["font.size"] = 10
 
 x = lens2_ensmean.time.dt.year.values
@@ -31,45 +33,10 @@ plt.xlabel('Time (yr)')
 plt.grid(ls='--', lw=0.4, color='grey')
 plt.xlim([1850, 2100])
 plt.xticks(rotation=0)
-#plt.ylim([24.5, 38.5])
 plt.legend(loc='upper right',prop={'size': 10})
 ax = plt.gca()
 ax.spines.right.set_visible(False)
 ax.spines.top.set_visible(False)
 ax.tick_params(direction="in")
+plt.savefig('../../../hyperdrought_data/png/LENS2_full_series.png', dpi=300, bbox_inches = 'tight', pad_inches = 0)
 plt.show()
-
-# for tick in ax.xaxis.get_major_ticks():
-#     tick.label.set_fontsize(10) 
-#     tick.label.set_weight('light') 
-# for tick in ax.yaxis.get_major_ticks():
-#     tick.label.set_fontsize(10) 
-#     tick.label.set_weight('light')
-
-# plt.sca(axs[1])
-# normfit_past = norm.fit(np.ravel(lens2_fixed_1928_2021.sel(time=slice('1851','1880')).values))
-# normfit_present = norm.fit(np.ravel(lens2_fixed_1928_2021.sel(time=slice('1991','2020')).values))
-# normfit_future = norm.fit(np.ravel(lens2_fixed_1928_2021.sel(time=slice('2071','2100')).values))
-# xmin, xmax = 24.5, 38.5
-# plt.ylim([0, 0.4])
-# plt.xlim([xmin, xmax])
-# xval = np.linspace(xmin, xmax, 100)
-# plt.plot(xval, norm.pdf(xval, *normfit_past), color='b', lw=1.5)
-# plt.plot(xval, norm.pdf(xval, *normfit_present), color='k', lw=1.5)
-# plt.plot(xval, norm.pdf(xval, *normfit_future), color='r', lw=1.5)
-# plt.axvline(np.mean(np.ravel(lens2_fixed_1928_2021.sel(time=slice('1991','2020')))) + qn_anom, lw=1.8, c='grey')
-# plt.grid(ls='--', lw=0.4, color='grey')
-# plt.ylabel('PDF')
-# plt.xlabel('January Tmax (ºC)')
-# ax = plt.gca()
-# ax.spines.right.set_visible(False)
-# ax.spines.top.set_visible(False)
-# ax.tick_params(direction="in")
-# for tick in ax.xaxis.get_major_ticks():
-#     tick.label.set_fontsize(10) 
-#     tick.label.set_weight('light') 
-# for tick in ax.yaxis.get_major_ticks():
-#     tick.label.set_fontsize(10) 
-#     tick.label.set_weight('light')
-# plt.tight_layout()
-# plt.savefig('../../../megafires_data/png/LENS2_timeseries_QNWE.png', dpi=300, bbox_inches = 'tight', pad_inches = 0)
