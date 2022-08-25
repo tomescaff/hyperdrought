9029289cb88b2d00f025a71ffec388b813a631da
Tomás Carrasco Escaff <tomescaff@gmail.com>
2022-08-25 12:31:30 -0400

    added precip correlation between QN and CR2MET

--- processing/processing/utils.py
+++ processing/processing/utils.py
@@ -1,6 +1,9 @@
 import xarray as xr
 import pandas as pd
 import numpy as np
+from . import series as se
+from . import cr2met as cr2
+from scipy.stats import pearsonr
 
 # get return periods from arg as numpy array
 def get_return_periods(z, method='up'):
@@ -29,3 +32,26 @@ def get_return_periods(z, method='up'):
         tau[i] = 1/tail[i]
 
     return u, tau
+
+# get corr between QN and CR2MET
+def get_corr_QN_CR2MET():
+    qn = se.get_QN_annual_precip_long_record()
+    pr = cr2.get_cr2met_annual_precip()
+    
+    qn = qn.sel(time=slice('1979','2019'))
+    pr = pr.sel(time=slice('1979','2019'))
+
+    t, n, m = pr.values.shape
+    matrix = np.zeros((n,m))
+    for i in range(n):
+        for j in range(m):
+            series = pr[:,i,j].values
+            if np.isnan(series[0]):
+                matrix[i,j] = np.nan
+            else:
+                r, p = pearsonr(series, qn.values)
+                matrix[i,j] = r
+    
+    return xr.DataArray(matrix, coords=[pr.lat, pr.lon], dims=['lat', 'lon'])
+
+
